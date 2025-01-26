07f79193bc36d426d64779111d32e486c7e94179
PabloChristianno <pablochristianno@gmail.com>
2025-01-26 10:49:54 -0300

    fix: Parse error

--- src/CellAgent.java
+++ src/CellAgent.java
@@ -17,21 +17,20 @@ public class CellAgent extends Agent {
     protected void setup() {
         System.out.println(getLocalName() + ": inicializando CellAgent...");
 
-        // Recupera as coordenadas do agente dos argumentos
+        // Recupera as coordenadas do agente
         Object[] args = getArguments();
         if (args == null || args.length < 2) {
             System.err.println("Erro: Coordenadas não foram passadas para " + getLocalName());
-            doDelete(); // Finaliza o agente se os argumentos estiverem ausentes
+            // Finaliza o agente
+            doDelete(); 
             return;
         }
         int x = Integer.parseInt(args[0].toString());
         int y = Integer.parseInt(args[1].toString());
 
-        // Registra no DF com as coordenadas
         registerOnDF(x, y);
 
         System.out.println(getLocalName() + " iniciado nas coordenadas (" + x + ", " + y + "). Estado inicial: " + isAlive);
-        isAlive = Math.random() < 0.3; // Define aleatoriamente o estado inicial
         addBehaviour(new VerifyNeighbor());
     }
 
@@ -92,9 +91,9 @@ public class CellAgent extends Agent {
                 if (previousState != isAlive) {
                     if (isAlive) {
                     	// Coordenadas do agente atual
-                        String[] position = getLocalName().split(",");
-                        int myX = Integer.parseInt(position[0].trim());
-                        int myY = Integer.parseInt(position[1].trim());
+                        String[] position = getLocalName().split("-");
+                        int myX = Integer.parseInt(position[1].trim());
+                        int myY = Integer.parseInt(position[2].trim());
                         registerOnDF(myX, myY);
                     } else {
                         deregisterFromDF();
@@ -106,7 +105,7 @@ public class CellAgent extends Agent {
                 response.setOntology("verifyIsAliveResponse");
                 response.setContent(Boolean.toString(isAlive));
                 send(response);
-                System.out.println(getLocalName() + " próximo estado: " + isAlive);
+                // System.out.println(getLocalName() + " próximo estado: " + isAlive); // Debuga o estado enviado
             } else {
                 block();
             }
@@ -116,26 +115,26 @@ public class CellAgent extends Agent {
             int count = 0;
 
             try {
-                // Configurar a busca no DF
+
                 DFAgentDescription template = new DFAgentDescription();
                 ServiceDescription sd = new ServiceDescription();
                 sd.setType("CellAgent");
                 template.addServices(sd);
 
-                // Recuperar todos os agentes do tipo "CellAgent"
+                // Recuperar todos os agentes célula
                 DFAgentDescription[] result = DFService.search(myAgent, template);
 
                 for (DFAgentDescription agentDesc : result) {
                 	ServiceDescription service = (ServiceDescription) agentDesc.getAllServices().next();
                     String[] position = service.getName().split(",");
-
+  
                     int x = Integer.parseInt(position[0].trim());
                     int y = Integer.parseInt(position[1].trim());
 
                     // Coordenadas do agente atual
-                    String[] myPosition = getLocalName().split(",");
-                    int myX = Integer.parseInt(myPosition[0].trim());
-                    int myY = Integer.parseInt(myPosition[1].trim());
+                    String[] myPosition = getLocalName().split("-");
+                    int myX = Integer.parseInt(myPosition[1].trim());
+                    int myY = Integer.parseInt(myPosition[2].trim()); 
 
                     // Verificar se o agente está ao redor (8 vizinhos)
                     if (Math.abs(x - myX) <= 1 && Math.abs(y - myY) <= 1 && !(x == myX && y == myY)) {
